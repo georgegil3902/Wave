09f210447a39e4e6717f34448a70ce63e5ef8979
George Gil <georgegil3902@gmail.com>
2024-09-28 10:51:36 +0200

    updated main file

--- Wave.py
+++ Wave.py
@@ -1,5 +1,7 @@
-from desktop.WaveGui import runui
-
+from desktop.WaveGui import initiate_ui
+from backend.mediaplayer import MediaPlayer
 
 if __name__=="__main__":
-    runui()
+    player = MediaPlayer()
+    player.load_media('backend/song.mp3')
+    initiate_ui(player)
